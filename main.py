c495c25643f9f3f83b724d0fd31b4d88a11e76a4
Johnny <JohnnyLieu@yahoo.com>
2022-03-06 16:41:56 -0800

    changed it a little

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-class Car:
+class Dealership:
     def __init__(self, year, make, model):
         self.year = year
         self.make = make
         self.model = model
 
-class Dealership:
+class Car:
     def __init__(self, owner, year, make, model):
         self.owner = owner
-        self.car = Car(year, make, model)
+        self.car = Dealership(year, make, model)
 
-        print(f"I am a {self.car.year} {self.car.make} {self.car.model}. I am owned by {owner}.")
+    def speak(self):
+        print(f"I am a {self.car.year} {self.car.make} {self.car.model}. I am owned by {self.owner}.")
 
-mercedes = Dealership("Johnny", "2023", "Mercedes", "Maybach S600 AMG")
-tesla = Dealership("Tyler", "2023", "Tesla", "X")
-beamer = Dealership("Jonathan", "2023", "BWM", "7 Series")
+Johnny = Car("Johnny", "2023", "Mercedes", "Maybach S600 AMG").speak()
 
 
 # Composition HW
