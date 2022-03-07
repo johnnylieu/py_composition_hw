9f2d743a172edb4ae4346ffd4eae4a49022023c1
Johnny <JohnnyLieu@yahoo.com>
2022-03-06 16:53:27 -0800

    corrected the print syntax

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Dealership:
         self.car = Car(year, make, model)
 
     def speak(self):
-        print(f"I am a {self.dealership.year} {self.dealership.make} {self.dealership.model}. I am owned by {self.owner}.")
+        print(f"I am a {self.car.year} {self.car.make} {self.car.model}. I am owned by {self.owner}.")
 
 Johnny = Dealership("Johnny", "2023", "Mercedes", "Maybach S600 AMG").speak()
 
