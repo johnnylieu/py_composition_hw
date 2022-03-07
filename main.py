9e71110bd5676e19027fdd64dc699f1651e5cac4
Johnny <JohnnyLieu@yahoo.com>
2022-03-06 16:30:27 -0800

    added print statement

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ class Dealership:
         self.owner = owner
         self.car = Car(year, make, model)
 
-        print(f"New Owner: {self.owner}\n")
-        print(f"I am a {self.car.year} {self.car.make} {self.car.model}.")
+        print(f"I am a {self.car.year} {self.car.make} {self.car.model}. I am owned by {owner}.")
 
 mercedes = Dealership("Johnny", "2023", "Mercedes", "Maybach S600 AMG")
 tesla = Dealership("Tyler", "2023", "Tesla", "X")
