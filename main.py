fa743efc45e0c483e4910695654097cf04e45041
Johnny <JohnnyLieu@yahoo.com>
2022-03-06 17:06:02 -0800

    composition

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ class Car:
         self.year = year
         self.make = make
         self.model = model
+    
+    def talk():
+        print(f"Thank you for visiting our dealership!\n")
 
 class Dealership:
     def __init__(self, owner, year, make, model):
@@ -10,9 +13,13 @@ class Dealership:
         self.car = Car(year, make, model)
 
     def speak(self):
-        print(f"I am a {self.car.year} {self.car.make} {self.car.model}. I am owned by {self.owner}.")
+        print(f"\n{self.owner} just bought our {self.car.year} {self.car.make} {self.car.model}.")
+    
+        return Car.talk()
 
 Johnny = Dealership("Johnny", "2023", "Mercedes", "Maybach S600 AMG").speak()
+Tyler = Dealership("Tyler", "2023", "Tesla", "Model X").speak()
+Jonathan = Dealership("Jonathan", "2023", "BWM", "6 Series").speak()
 
 
 # Composition HW
