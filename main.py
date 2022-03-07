addf6fee2e5cd3b566b2a7b7f7ba66ebe2d18a46
Johnny <JohnnyLieu@yahoo.com>
2022-03-06 16:28:58 -0800

    unsure if this is how composition is..

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-class Dealership:
+class Car:
     def __init__(self, year, make, model):
         self.year = year
         self.make = make
         self.model = model
 
-class Car:
+class Dealership:
     def __init__(self, owner, year, make, model):
         self.owner = owner
-        self.dealership = Dealership(year, make, model)
+        self.car = Car(year, make, model)
 
-        print(f"I am a {self.dealership.year} {self.dealership.make} {self.dealership.model} and I am owned by {self.owner}")
+        print(f"New Owner: {self.owner}\n")
+        print(f"I am a {self.car.year} {self.car.make} {self.car.model}.")
 
-
-mercedes = Car("Johnny", "2023", "Mercedes", "Maybach S600 AMG")
-tesla = Car("Tyler", "2023", "Tesla", "X")
-beamer = Car("Jonathan", "2023", "BWM", "7 Series")
+mercedes = Dealership("Johnny", "2023", "Mercedes", "Maybach S600 AMG")
+tesla = Dealership("Tyler", "2023", "Tesla", "X")
+beamer = Dealership("Jonathan", "2023", "BWM", "7 Series")
 
 
 # Composition HW
